2680d88cdcdea83611d0f570012d336f88fde204
wrightangles <ian.f.t.wright@gmail.com>
2017-08-26 16:54:52 -0400

    testing dynamo

--- application.py
+++ application.py
@@ -1,11 +1,32 @@
 from flask import Flask
+import boto3
 
 application = Flask(__name__)
 
 
 @application.route('/')
 def hello_world():
-    return 'Hello World!'
+    dynamodb = boto3.resource('dynamodb')
+    user_table = dynamodb.Table('USERS')
+
+    user_table.put_item(
+        Item={
+            'user_id': '2',
+            'first_name': 'Joe',
+            'last_name': 'Smith',
+            'age': 15
+        }
+    )
+
+    response = user_table.get_item(
+        Key={
+            'user_id': '2'
+        }
+    )
+    print(response)
+    item = response['Item']
+    print(str(item))
+    return 'Hello World!' + str(item)
 
 
 if __name__ == '__main__':
